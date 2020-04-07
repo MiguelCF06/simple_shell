8ff81392a6090370d918232abbbf30b25f7058ea
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-04-07 18:00:49 +0000

    Added prototype strlen

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int _strLen(char *s);
+
+#endif
