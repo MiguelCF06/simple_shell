c0122b2de4cc56f7c16224591fccdcda75e2cd46
juandsuarezz <1486@holbertonschool.com>
2020-04-15 19:42:39 +0000

    Error message added

--- p_error.c
+++ p_error.c
@@ -1,23 +1,26 @@
 #include "holberton.h"
 /**
- * p_error - prints error if the command is not found
- * @arg1: name of the executable file
- * @arg2: name of the command that not exists
- * @count: number of proccess
+ * p_error - prints error messages
+ * @vars: struct of variables
+ * @errormsg: number of proccess
 **/
 
-void p_error(char *arg1, char *arg2, int count)
+void p_error(vabs_st *vars, char *errormsg)
 {
 	char *numstr;
 
-	_print(arg1);
+	_print(vars->argv[0]);
 	_print(": ");
-	numstr = _itoa(count);
+	numstr = _itoa(vars->count);
 	_print(numstr);
 	_print(": ");
-	_print(arg2);
-	_print(": ");
-	write(STDERR_FILENO, "not found\n", 11);
+	_print(vars->cla[0]);
+	if (errormsg)
+	{
+		_print(errormsg);
+	}
+	else
+		perror("");
 }
 /**
  * _print - prints strings
