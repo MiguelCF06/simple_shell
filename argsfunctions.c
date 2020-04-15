4051b793b214e5b252cd6c2967384aca9bce88fc
juandsuarezz <1486@holbertonschool.com>
2020-04-15 20:54:58 +0000

    atoi function added

--- argsfunctions.c
+++ argsfunctions.c
@@ -34,3 +34,26 @@ char **parseString(char *buffer, char *separator)
 	}
 	return (tokens);
 }
+
+/**
+ * _atoi - string to integer
+ * @s: string to transform
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int n;
+	int sign = 1;
+	unsigned int res = 0;
+
+	for (n = 0; *(s + n) != '\0'; n++)
+	{
+		if (*(s + n) == '-')
+			sign *= -1;
+		if (*(s + n) >= '0' && *(s + n) <= '9')
+			res = res * 10 + (*(s + n) - '0');
+		else if (res > 0)
+			break;
+	}
+	return (res * sign);
+}
